be9aea53d0590d726fed9f3222cebe29d9030912
Fayed Gaya <gayafayed@gmail.com>
2024-11-24 06:06:59 -0700

    Add a basic controller and its test, add a basic service

---
+++ src/main/kotlin/com/superbank/superbank/controller/HelloController.kt
@@ -0,0 +1,16 @@
+package com.superbank.superbank.controller
+
+import com.superbank.superbank.service.HelloService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController (
+    private val helloService: HelloService
+){
+
+    @GetMapping("/")
+    fun home(): String {
+        return helloService.hello()
+    }
+}
