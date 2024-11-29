a7a6604c3ffe3f1f167177aebc5118f4f2491a70
Fayed Gaya <gayafayed@gmail.com>
2024-11-29 03:15:22 -0700

    Script docker initialization of Superbank

---
+++ docker-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build the Docker image
+docker build -t superbank .
+
+# Stop any running containers (if they exist)
+docker stop superbank-container || true
+
+# Remove the old container (if it exists)
+docker rm superbank-container || true
+
+# Run the Docker container
+docker run --name superbank-container -p 8080:8080 superbank
