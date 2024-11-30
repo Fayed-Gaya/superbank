67fa87f5abfa3c3b946fddf2214a122719650b4d
Fayed-Gaya <gayafayed@gmail.com>
2024-11-30 19:10:04 +0000

    SB-3: Test Jira Integration 4

--- src/main/kotlin/com/superbank/superbank/controller/HelloController.kt
+++ src/main/kotlin/com/superbank/superbank/controller/HelloController.kt
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController(
     private val helloService: HelloService
 ) {
-    // Test
     @GetMapping("/hello-world")
     fun home(): String {
         return helloService.hello()
